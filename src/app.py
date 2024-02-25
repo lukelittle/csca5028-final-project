48cbdd51b6811210f7a59372d7da771655bd09be
Lucas Little <702739+lukelittle@users.noreply.github.com>
2024-02-25 16:26:38 -0500

    debug

--- src/app.py
+++ src/app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    test_postgres = check_postgres_connection()
-    test_ironmq = check_ironmq_connection()
+    # test_postgres = check_postgres_connection()
+    # test_ironmq = check_ironmq_connection()
     return f'''
     <h1>{test_postgres}</h1>
     <h1>{test_ironmq}</h1>
