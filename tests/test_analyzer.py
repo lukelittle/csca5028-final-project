53250e8985fcb9c12866358b376650cb470904dd
Lucas Little <702739+lukelittle@users.noreply.github.com>
2024-02-25 16:21:44 -0500

    fixing tests

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,2 +1,7 @@
-def test_pass():
-    assert True
+import unittest
+
+# from analyzer import Analyzer
+
+class TestAnalyzer(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(True)
