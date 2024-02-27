2d005d0e2e270656df64caf5fec5ab83b8ea95d4
Lucas Little <702739+lukelittle@users.noreply.github.com>
2024-02-27 00:19:31 -0500

    run

--- app/run.py
+++ app/run.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template, request, redirect, url_for
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import os
-
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
@@ -34,24 +33,24 @@ class ThreeDayAverageVisibility(Base):
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or 'sqlite:///:memory:'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
-# @app.route("/query", methods=["GET"])
-# def query_visibility():
-#     station_code = request.args.get('station')
-#     if station_code:
-#         results = ThreeDayAverageVisibility.query.filter_by(station=station_code).all()
-#         return render_template('results.html', results=results, station=station_code)
-#     else:
-#         return redirect(url_for('main'))
+@app.route("/query", methods=["GET"])
+def query_visibility():
+    station_code = request.args.get('station')
+    if station_code:
+    
+        results = ThreeDayAverageVisibility.query.filter_by(station=station_code).all()
+        return render_template('results.html', results=results, station=station_code)
+    else:
+        return redirect(url_for('main'))
 
 if __name__ == "__main__":
-    # db.create_all()  
-    app.run(debug=True) 
+    db.create_all()
+    app.run(debug=True)
