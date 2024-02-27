7c21bb6d61f5374e3430a10db011bfd6ac675a07
Lucas Little <702739+lukelittle@users.noreply.github.com>
2024-02-26 21:25:46 -0500

    temp fix

--- app/app.py
+++ app/app.py
@@ -9,28 +9,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    test_postgres = check_postgres_connection()
-    test_ironmq = check_ironmq_connection()
-    return render_template('index.html', test_postgres=test_postgres, test_ironmq=test_ironmq)
-
-def check_postgres_connection():
-    try:
-        DATABASE_URL = os.environ.get("DATABASE_URL")
-        conn = psycopg2.connect(DATABASE_URL)
-        cur = conn.cursor()
-        cur.execute('SELECT 1')
-        cur.close()
-        conn.close()
-        return "Successful"
-    except Exception as e:
-        return f"Failed - {e}"
-
-def check_ironmq_connection():
-    try:
-        ironmq = IronMQ(host="mq-aws-eu-west-1-1.iron.io", project_id=os.environ.get("IRONMQ_PROJECT_ID"), token=os.environ.get("IRONMQ_TOKEN"))
-        queue = ironmq.queue("csca5028")
-        queue.post("Test message")
-        info = queue.info()
-        return "Successful" if info else "Failed"
-    except Exception as e:
-        return f"Failed - {e}"
+    return render_template('index.html')
